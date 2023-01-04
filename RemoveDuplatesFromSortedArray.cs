ca6bf46ee42c19552ed01e99ba838b8c7e289a50
Radostin Atanasov <16541785+RadostinAtansov@users.noreply.github.com>
2023-01-04 22:04:03 +0200

    Add files via upload

---
+++ RemoveDuplatesFromSortedArray.cs
@@ -0,0 +1,18 @@
+﻿
+int[] nums1 = { 1, 1, 2 };
+//Output: 2, nums = [1,2,_]
+int[] nums2 = {0,0,1,1,1,2,2,3,3,4};
+//Output: 5, nums = [0, 1, 2, 3, 4, _, _, _, _, _]
+
+int k = RemoveDuplicates(nums1);
+
+static int RemoveDuplicates(int[] nums)
+{
+    int i = 1;
+    foreach (int n in nums)
+    {
+        if (nums[i - 1] != n) nums[i++] = n;
+    }
+
+    return i;
+}
