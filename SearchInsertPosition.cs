15e6a57f5c3a8db7637d2f08ecaef11306c67456
Radostin Atanasov <16541785+RadostinAtansov@users.noreply.github.com>
2023-01-06 21:22:37 +0200

    Add files via upload

---
+++ SearchInsertPosition.cs
@@ -0,0 +1,40 @@
+﻿
+int[] nums1 = { 1, 3, 5, 6 };
+int target1 = 5;
+//Output: 2
+int target2 = 2;
+//Output: 1
+int target3 = 7;
+//Output: 4
+int[] nums2 = { 1 };
+
+
+Console.WriteLine(SearchInsert(nums2, 0));
+
+static int SearchInsert(int[] nums, int target)
+{
+	int[] resultSeq = nums.Where(s => s.Equals(target)).ToArray();
+	if (resultSeq.Length > 0)
+	{
+		for (int i = 0; i < nums.Length; i++)
+		{
+			if (nums[i] == target)
+			{
+				return i;
+			}
+		}
+			
+	}
+	else if (resultSeq.Length == 0)
+	{
+		for (int i = 0; i < nums.Length; i++)
+		{
+			if (target < nums[i])
+			{
+				return i;
+			}
+		}
+	}
+
+    return nums.Length;
+}
