a1a1f443e60949fb8c96f3612418805541816700
Radostin Atanasov <16541785+RadostinAtansov@users.noreply.github.com>
2023-01-06 21:42:42 +0200

    Add files via upload

---
+++ LengthOfLastWord.cs
@@ -0,0 +1,17 @@
+﻿string s1 = "Hello World";
+//Output: 5
+string s2 = "   fly me   to   the moon  ";
+//Output: 4
+string s3 = "luffy is still joyboy";
+//Output: 6
+
+Console.WriteLine(LengthOfLastWord(s3));
+
+static int LengthOfLastWord(string s)
+{
+    string[] splitString = s.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+    string lastWord = splitString[splitString.Length - 1];
+
+    return lastWord.Length;
+}
