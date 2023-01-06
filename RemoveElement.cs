f1a21788372ecb659ccc52b78e1227dfc55c18d6
Radostin Atanasov <16541785+RadostinAtansov@users.noreply.github.com>
2023-01-06 18:24:47 +0200

    Add files via upload

---
+++ RemoveElement.cs
@@ -0,0 +1,27 @@
+﻿int[] nums1 = { 3, 2, 2, 3 };
+int val1 = 3;
+//Output: 2, nums = [2, 2, _, _]
+
+int[] nums2 = { 0, 1, 2, 2, 3, 0, 4, 2 };
+int val2 = 2;
+//Output: 5, nums = [0, 1, 4, 0, 3, _, _, _]
+int result = RemoveElement(nums2, val2);
+
+Console.WriteLine(result);
+
+static int RemoveElement(int[] nums, int val)
+{
+	int counter = 0;
+	int index = 0;
+
+    for (int i = 0; i < nums.Length; i++) // on place algorithm
+	{
+		if (nums[i] != val)
+		{
+			nums[index++] = nums[i];
+			counter++;
+		}
+	}
+
+    return counter;
+}
