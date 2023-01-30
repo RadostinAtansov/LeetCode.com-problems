90fa5e08ce26a8ffb9c9fe6e644ea62882e9cc3e
Radostin Atanasov <16541785+RadostinAtansov@users.noreply.github.com>
2023-01-30 21:39:26 +0200

    Add files via upload

---
+++ SingleNumber.cs
@@ -0,0 +1,47 @@
+﻿
+
+int[] nums1 = { 2, 2, 1 };
+//Output: 1
+
+int[] nums2 = { 4, 1, 2, 1, 2 };
+//Output: 4
+
+int[] nums3 = { 1 };
+//Output: 1
+
+Console.WriteLine(SingleNumber(nums2));
+
+static int SingleNumber(int[] nums)
+{
+	int result = 0;
+
+	for (int i = 0; i < nums.Length; i++)
+	{
+		result = result ^ nums[i];
+	}
+
+	//int count = 0;
+	//int result = 0;
+
+	//for (int i = 0; i < nums.Length; i++)
+	//{
+
+	//	for (int l = 0; l < nums.Length; l++)
+	//	{
+	//		if (nums[i] == nums[l])
+	//		{
+	//			count++;
+	//		}
+	//	}
+
+	//	result = nums[i];
+
+	//	if (count == 1)
+	//	{
+	//		break;
+	//	}
+	//	count = 0;
+	//}
+
+    return result;
+}
