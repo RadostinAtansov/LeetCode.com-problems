64a24da7c20a5b7ced1b4106cd803caaffa51fd0
Radostin Atanasov <16541785+RadostinAtansov@users.noreply.github.com>
2023-02-18 17:58:57 +0200

    Add files via upload

---
+++ RangeSumQuery_Immutable.cs
@@ -0,0 +1,39 @@
+﻿
+
+//Example 1:
+
+//Input
+//["NumArray", "sumRange", "sumRange", "sumRange"]
+
+//Output
+//[null, 1, -1, -3]
+
+//Explanation
+//NumArray numArray = new NumArray([-2, 0, 3, -5, 2, -1]);
+//numArray.sumRange(0, 2); // return (-2) + 0 + 3 = 1
+//numArray.sumRange(2, 5); // return 3 + (-5) + 2 + (-1) = -1
+//numArray.sumRange(0, 5); // return (-2) + 0 + 3 + (-5) + 2 + (-1) = -3
+
+public class NumArray
+{
+	private int[] nums;
+
+	public NumArray(int[] nums)
+	{
+		this.nums = nums;
+	}
+
+
+    public int SumRange(int left, int right)
+    {
+		int sum = 0;
+
+		for (int i = left; i <= right; i++)
+		{
+			sum += this.nums[i];
+		}
+
+        return sum;
+    }
+
+}
