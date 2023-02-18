8996b22745c044b003c91eade655da3e1f0ac3fe
Radostin Atanasov <16541785+RadostinAtansov@users.noreply.github.com>
2023-02-18 22:49:26 +0200

    Add files via upload

---
+++ ReverseString.cs
@@ -0,0 +1,19 @@
+﻿
+
+
+//Example 1:
+
+char[] s1 = { 'h', 'e', 'l', 'l', 'o' };
+//Output: ["o","l","l","e","h"]
+
+//Example 2:
+
+char[] s2 = { 'H', 'a', 'n', 'n', 'a', 'h' };
+//Output: ["h","a","n","n","a","H"]
+
+ReverseString(s1);
+
+static void ReverseString(char[] s)
+{
+   Array.Reverse(s);
+}
