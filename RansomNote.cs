ddd6e4d5ef900381f1fb158a25255689a8e148c0
Radostin Atanasov <16541785+RadostinAtansov@users.noreply.github.com>
2023-02-24 13:17:15 +0200

    Add files via upload

---
+++ RansomNote.cs
@@ -0,0 +1,38 @@
+﻿
+
+//Example 1:
+string ransomNote1 = "a", magazine1 = "b";
+//Output: false
+
+//Example 2:
+string ransomNote2 = "aa", magazine2 = "ab";
+//Output: false
+
+//Example 3:
+string ransomNote3 = "aa", magazine3 = "aab";
+//Output: true
+
+
+Console.WriteLine(CanConstruct(ransomNote1, magazine1));
+
+static bool CanConstruct(string ransomNote, string magazine)
+{
+	List<char> listCharsMagazine = new List<char>(magazine);
+	List<char> ransomNoteRansomNote = new List<char>(ransomNote);
+
+    for (int i = 0; i < ransomNoteRansomNote.Count; i++)
+	{
+		char c = ransomNoteRansomNote[i];
+
+		if (listCharsMagazine.Contains(c))
+		{
+			listCharsMagazine.Remove(c);     //==========> work with string are slow
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+    return true;
+}
