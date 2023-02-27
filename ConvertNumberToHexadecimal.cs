adcaf50bb7a178c94764b04d04618dfdd4bd0548
Radostin Atanasov <16541785+RadostinAtansov@users.noreply.github.com>
2023-02-27 22:17:36 +0200

    Add files via upload

---
+++ ConvertNumberToHexadecimal.cs
@@ -0,0 +1,16 @@
+﻿
+
+//Example 1:
+int num1 = 26;
+//Output: "1a"
+
+//Example 2:
+int num2 = -1;
+//Output: "ffffffff"
+
+Console.WriteLine(ToHex(num1));
+
+static string ToHex(int num)
+{
+    return num.ToString("X").ToLower();
+}
