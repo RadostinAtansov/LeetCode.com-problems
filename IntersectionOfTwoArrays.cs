68d753b710e29fc6eb38dede6e4befb5d6ff1225
Radostin Atanasov <16541785+RadostinAtansov@users.noreply.github.com>
2023-03-01 23:48:03 +0200

    Add files via upload

---
+++ IntersectionOfTwoArrays.cs
@@ -0,0 +1,37 @@
+﻿
+
+
+//Example 1:
+int[] nums11 = { 1, 2, 2, 1 }, nums21 = { 2, 2 };
+//Output: [2]
+
+//Example 2:
+int[] nums12 = { 4, 9, 5 }, nums22 = { 9, 4, 9, 8, 4 };
+//Output: [9,4]
+//Explanation: [4,9] is also accepted.
+
+var result = Intersection(nums11, nums21);
+
+Console.WriteLine(String.Join(",", result.Distinct()));
+
+static int[] Intersection(int[] nums1, int[] nums2)
+{
+    List<int> result = new List<int>();
+    List<int> result1 = new List<int>(nums1);
+    List<int> result2 = new List<int>(nums2);
+    var num1 = result1.Distinct();
+    var num2 = result2.Distinct();
+
+    for (int i = 0; i < result1.Count; i++)
+    {
+        int resN = result1[i];
+        if (result2.Contains(resN))
+        {
+            result.Add(resN);
+        }
+    }
+
+    var res = result.Distinct();
+
+    return res.ToArray();
+}
