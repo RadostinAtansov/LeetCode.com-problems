166055be027f4cafcca879da4fc84941f2a81f00
Radostin Atanasov <16541785+RadostinAtansov@users.noreply.github.com>
2023-03-23 13:47:57 +0200

    Add files via upload

---
+++ ReverseWordsInAString3.cs
@@ -0,0 +1,36 @@
+﻿
+
+//Given a string s, reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
+
+
+
+//Example 1:
+
+string s1 = "Let's take LeetCode contest";
+//Output: "s'teL ekat edoCteeL tsetnoc"
+//Example 2:
+
+string s2 = "God Ding";
+//Output: "doG gniD"
+
+Console.WriteLine(ReverseWords(s1));
+
+static string ReverseWords(string s)
+{
+	string[] strSplit = s.Split(" ");
+
+	for (int i = 0; i < strSplit.Length; i++)
+	{
+		string strWord = strSplit[i];
+		char[] word = strSplit[i].ToCharArray();
+		Array.Reverse(word);
+		string newWordReversed = new string(word);
+
+		strSplit[i] = newWordReversed;
+	}
+
+
+
+
+    return string.Join(" ", strSplit);
+}
