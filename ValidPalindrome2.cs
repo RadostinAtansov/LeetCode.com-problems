76752fab36a06c7359c0fff9b4ce2657604760ba
Radostin Atanasov <16541785+RadostinAtansov@users.noreply.github.com>
2023-04-04 20:42:07 +0300

    Add files via upload

---
+++ ValidPalindrome2.cs
@@ -0,0 +1,50 @@
+﻿
+
+//Given a string s, return true if the s can be palindrome after deleting at most one character from it.
+
+//Example 1:
+string s1 = "aba";
+//Output: true
+
+//Example 2:
+string s2 = "abca";
+//Output: true
+//Explanation: You could delete the character 'c'.
+
+//Example 3:
+string s3 = "abcdefg";
+//Output: false
+
+string test = "deeee";
+
+Console.WriteLine(ValidPalindrome(test));
+
+static bool ValidPalindrome(string s)
+{
+    int left = 0, right = s.Length - 1;
+
+    while (left < right)
+    {
+        if (s[left] == s[right])
+        {
+            left++;
+            right--;
+        }
+        else
+            return IsPalindrome(s, left + 1, right) || IsPalindrome(s, left, right - 1);
+    }
+
+    return true;
+}
+
+static bool IsPalindrome(string s, int left, int right)
+{
+    while (left < right)
+    {
+        if (s[left] != s[right])
+            return false;
+        left++;
+        right--;
+    }
+    return true;
+}
