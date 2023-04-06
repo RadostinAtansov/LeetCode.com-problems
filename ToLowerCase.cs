795d8a5cc4893bf6e30585216d87da3094d9f2c1
Radostin Atanasov <16541785+RadostinAtansov@users.noreply.github.com>
2023-04-06 22:46:34 +0300

    Add files via upload

---
+++ ToLowerCase.cs
@@ -0,0 +1,23 @@
+﻿//Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+
+
+
+//Example 1:
+string s1 = "Hello";
+//Output: "hello"
+
+//Example 2:
+string s2 = "here";
+//Output: "here"
+
+string s3 = "LOVELY";
+//Output: "lovely"
+
+Console.WriteLine(ToLowerCase(s1));
+
+
+static string ToLowerCase(string s)
+{
+
+    return s.ToLower();
+}
